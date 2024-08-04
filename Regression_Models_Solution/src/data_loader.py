cc9046c5e43912aec884d352062680e4b270dce9
Tanvi Gera <138350796+tanvi3018@users.noreply.github.com>
2024-08-04 12:27:56 -0400

    Delete Regression_Models_Solution directory

--- Regression_Models_Solution/src/data_loader.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def load_data(file_path):
-    data = pd.read_csv(file_path)
-    X = data[['feature1', 'feature2']]
-    y = data['target']
-    return X, y
--- Regression_Models_Solution/src/model_evaluator.py
+++
@@ -1,6 +0,0 @@
-from sklearn.metrics import mean_squared_error
-
-def evaluate_model(model, X_test, y_test):
-    predictions = model.predict(X_test)
-    mse = mean_squared_error(y_test, predictions)
-    return mse
